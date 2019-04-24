11b5d39c325e3e8968f0de2a9f6071c8312a3990
Maxim <max281@icloud.com>
2019-04-24 21:02:49 +0300

    project structure created

---
+++ src/main/java/com/alekhnovich/maxim/testyellowsoftproject/TestyellowsoftprojectApplication.java
@@ -0,0 +1,13 @@
+package com.alekhnovich.maxim.testyellowsoftproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestyellowsoftprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestyellowsoftprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alekhnovich/maxim/testyellowsoftproject/TestyellowsoftprojectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.alekhnovich.maxim.testyellowsoftproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestyellowsoftprojectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
